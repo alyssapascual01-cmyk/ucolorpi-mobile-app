4e4a263940151f5b46ed85229e6ae9cc5fc90d2d
ralphjacobroque <alyssa.pascual01@gmail.com>
2026-01-13 21:55:48 +0800

    firebase configurations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.ui"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 19
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
@@ -44,4 +44,13 @@ android {
 
 flutter {
     source = "../.."
+}
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
